2d6a08176e3061b83a1b7a39b58410b9b1fe4109
Jsywd <navaratmint24@gmail.com>
2026-04-24 17:10:56 +0700

    commit

--- dontforget/backend/server.js
+++ dontforget/backend/server.js
@@ -22,9 +22,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-  secure: process.env.NODE_ENV === 'production',
-  maxAge: 7 * 24 * 60 * 60 * 1000 
-} // 7 days
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000 
+  }
 }));
 
 app.use(passport.initialize());
